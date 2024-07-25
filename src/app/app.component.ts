ec6a6aee626ef7ddaf098bbaadd31f4382b632bd
Bruno Silva <138171223+3run0Silva@users.noreply.github.com>
2024-07-25 09:33:29 +0200

    Docs: added github page config files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import {
+  IonApp,
+  IonRouterOutlet,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButtons,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [IonRouterOutlet, IonApp, RouterOutlet],
+  imports: [
+    IonButtons,
+    IonContent,
+    IonTitle,
+    IonToolbar,
+    IonHeader,
+    IonRouterOutlet,
+    IonApp,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,14 +1,9 @@
-import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
-  standalone: true,
-  imports: [IonicModule, CommonModule],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePageComponent implements OnInit {
   constructor() {}
