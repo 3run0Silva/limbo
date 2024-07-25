4b14598eb40a0ec7a0c6cbc37ff3f983695f656c
Bruno Silva <138171223+3run0Silva@users.noreply.github.com>
2024-07-25 13:01:36 +0200

    Feat: created the basic layout for Home Page
    - Added some basic ionic layout structure to the Home Page.
    - Created some hard coded data
    
    Files modified:
    - src\app\pages\gallery-page\gallery-page.component.html
    - src\app\pages\gallery-page\gallery-page.component.ts
    - src\app\pages\home-page\home-page.component.html
    - src\app\pages\home-page\home-page.component.ts
    - src\app\pages\nightlife-page\nightlife-page.component.html
    - src\app\pages\nightlife-page\nightlife-page.component.ts
    
    Author: Bruno Silva
    Date: Thu, Jul 25, 2024  1:01:35 PM

--- src/app/pages/nightlife-page/nightlife-page.component.ts
+++ src/app/pages/nightlife-page/nightlife-page.component.ts
@@ -1,14 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-nightlife-page',
+  standalone: true,
+  imports: [CommonModule, IonicModule],
   templateUrl: './nightlife-page.component.html',
   styleUrls: ['./nightlife-page.component.scss'],
 })
-export class NightlifePageComponent  implements OnInit {
+export class NightlifePageComponent implements OnInit {
+  institutions = [
+    { name: 'Bar ABC', status: 'open' },
+    { name: 'Club XYZ', status: 'closed' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
 }
